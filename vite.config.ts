338413df56c1d9f404b043b1a24e9324aa8c80e4
kiw <106908925+Ikiw97@users.noreply.github.com>
2025-12-03 12:57:16 +0700

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(async ({ mode }) => {
+  const plugins = [react()];
+
+  // Only load express plugin in development mode
+  if (mode === "development") {
+    const { expressPlugin } = await import("./vite.dev-plugin");
+    plugins.push(expressPlugin());
+  }
+
+  return {
+    server: {
+      host: "::",
+      port: 8080,
+      fs: {
+        allow: ["./client", "./shared"],
+        deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
+      },
+      middlewareMode: false,
+    },
+    build: {
+      outDir: "dist/spa",
+    },
+    plugins,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./client"),
+        "@shared": path.resolve(__dirname, "./shared"),
+      },
+    },
+  };
+});
